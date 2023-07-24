1b780854fb5e8d4b4d4c6bef67b2232aa5bf5918
Miroslav Vadkerti <mvadkert@redhat.com>
2023-07-24 11:39:02 +0200

    Add update command
    This functionality was hidden, as it is handled by the wrapper shell
    script.
    
    Let's expose it to the user, so he knows it is there.
    
    Make sure the command appears only in case of the container execution.
    To indicate the execution, use a simple file in the container.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- src/tft/cli/tool.py
+++ src/tft/cli/tool.py
@@ -1,9 +1,12 @@
 # Copyright Contributors to the Testing Farm project.
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+
 import typer
 
 import tft.cli.commands as commands
+from tft.cli.config import settings
 
 app = typer.Typer()
 
@@ -13,3 +16,7 @@ app.command()(commands.reserve)
 app.command()(commands.run)
 app.command()(commands.version)
 app.command()(commands.watch)
+
+# This command is available only for the container based deployment
+if os.path.exists(settings.CONTAINER_SIGN):
+    app.command()(commands.update)
